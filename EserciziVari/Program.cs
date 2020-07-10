6e1316543c3b8876c4d6dd4ede35131d231f3286
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-10 13:01:52 +0200

    Utilizzo parola this

--- EserciziVari/Program.cs
+++ EserciziVari/Program.cs
@@ -10,35 +10,31 @@ namespace EserciziVari
     {
         static void Main(string[] args)
         {
-            var p = new Persona();
-            Console.WriteLine(p.Counter);
-            Console.WriteLine(p.Counter);
-            Console.WriteLine(p.Counter);
-            var p2 = new Persona();
-            Console.WriteLine(p2.MyProperty);
-            Console.WriteLine(p2.MyProperty);
-            Console.WriteLine(p2.MyProperty);
-            Persona.counter++;
-            Console.WriteLine(p.Counter);
-            Console.WriteLine(p2.MyProperty);
+            var p = new Persona("valdes");
+            p.GetNettoGuadagnato();
+
         }
     }
 
     public class Persona
     {
-        public static int counter;
-
-        //ExpressionBody
-        public int Counter => counter++;
-
-        //Auto-implemented property
-        public int MyProperty { get; } = counter++;
-
-        public Persona()
+        private readonly string cognome;
+        public int Reddito { get; set; } = 5000;
+        public Persona(string cognome)
         {
-
+            this.cognome = cognome;
         }
+        public double GetNettoGuadagnato()
+        {
+            return Reddito - TaxCalculator.GetTaxes(this);
+        }
+    }
 
-
+    public class TaxCalculator
+    {
+        public static double GetTaxes(Persona pers)
+        {
+            return pers.Reddito * 0.27;
+        }
     }
 }
