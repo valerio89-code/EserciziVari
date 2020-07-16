afd3f03aa8805ecd814041b9ae7dad7bcb4de093
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-16 12:10:42 +0200

    differenza tra ref e out

--- EserciziVari/Program.cs
+++ EserciziVari/Program.cs
@@ -12,10 +12,72 @@ namespace EserciziVari
     {
         static void Main(string[] args)
         {
-            var complesso = new Complesso();
-            complesso.Suonatori.Add(new Batteria());
-            //verificare differenza tra setter e utilizzo property; inizializzazione vs expression body
-            complesso.Suonatori = new List<ISuonatore>();
+            var a = new A();
+            double myMediaOut;
+            var str2 = a.MediaConOut(out myMediaOut, 6, 7, 8, 9);
+
+            double myMediaRef = 10;
+            var str = a.MediaConRef(ref myMediaRef, 6, 7, 8, 9);
+
+            var b = new B() { Nome = "gianlupo" };
+            a.ValorizzaClasse(ref b);
+            Console.WriteLine(b.Nome);
         }
     }
+
+    public class A
+    {
+        public string Media(double media, params int[] values)
+        {
+            //var media = valore chhe gli ho passato
+            var somma = 0;
+            foreach (var intero in values)
+            {
+                somma += intero;
+            }
+            media = (double)somma / values.Length;
+            return "media calcolata";
+        }
+
+        public string MediaConOut(out double media, params int[] values)
+        {
+            var somma = 0;
+            foreach (var intero in values)
+            {
+                somma += intero;
+            }
+            //myMediaConOut
+            media = (double)somma / values.Length;
+            return "media calcolata";
+        }
+        public string MediaConRef(ref double media, params int[] values)
+        {
+            var somma = 0;
+            foreach (var intero in values)
+            {
+                somma += intero;
+            }
+            //myMediaConOut
+            media = (double)somma / values.Length;
+            return "media calcolata";
+        }
+
+        public bool ValorizzaClasse(ref B cl)
+        {
+            cl = new B();
+            cl.Nome = "luca";
+            return true;
+        }
+    }
+
+    public class B
+    {
+        public string Nome { get; set; }
+
+
+    }
+
+
+
+
 }
