59bf19680739d8628e7b530d1fd754d660dc5b31
Valerio Valdes <valerio.valdes89@outlook.com>
2020-07-21 11:03:25 +0200

    classi annidate

--- EserciziVari/Program.cs
+++ EserciziVari/Program.cs
@@ -1,82 +1,25 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static EserciziVari.Classe1;
 
 namespace EserciziVari
 {
-    public static class Extensions
-    {
-        public static void StampaValori(this A istanzaAAmiuscolo)
-        {
-            foreach (var item in istanzaAAmiuscolo.ultimiValoriUsatiNelCalcoloMedia)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        public static void StampaValoriLista<T>(this List<T> listaDiInteri)
-        {
-            foreach (var item in listaDiInteri)
-            {
-                Console.WriteLine(item.ToString());
-            }
-        }
-
-        public static void StampaValoriArray<T>(this T[] array)
-        {
-
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var list = new List<int> { 1, 2, 3, 4, 6, 7, 42 };
-            Extensions.StampaValoriLista(list);
-            list.StampaValoriLista();
-            list.Average();
-            list.Add(19);
-
-
-
-            A a = new A();
-            Console.WriteLine(a.Media(2, 1, 2, 3));
-            foreach (var valore in a.ultimiValoriUsatiNelCalcoloMedia)
-            {
-                Console.WriteLine(valore);
-            }
-            Console.WriteLine(a.Media(2, 1, 2, 4, 5, 8, 9, 10, 25));
-            foreach (var valore in a.ultimiValoriUsatiNelCalcoloMedia)
-            {
-                Console.WriteLine(valore);
-            }
-            a.StampaValori();
-
-            Extensions.StampaValori(a);
-            a.StampaValori();
-
-
-
-        }
+            var c1 = new Classe1();
+            var c2 = new Classe2();
+        }       
     }
 
-    public class A
+    public class Classe1
     {
-        public int[] ultimiValoriUsatiNelCalcoloMedia;
-
-        public string Media(params int[] values)
+        private Classe2 MyPropertyc1 { get; set; } = new Classe2();
+        public class Classe2
         {
-            ultimiValoriUsatiNelCalcoloMedia = values;
-            var somma = 0;
-            foreach (var intero in values)
-            {
-                somma += intero;
-            }
-            var media = (double)somma / values.Length;
-            return $"media calcolata {media}";
+            public int MyPropertyc2 { get; set; }
+
         }
     }
+
+
 }
